af0335214de1102edd0684d30f66fc187a5faa67
= <=>
2022-05-31 02:04:08 +0200

    Added Some Util functions

--- app/src/main/java/com/salma/apitester/MainActivity.kt
+++ app/src/main/java/com/salma/apitester/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.salma.apitester
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.TextView
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         }
         testBtn.setOnClickListener {
             val selectedRequest = radioGroup.checkedRadioButtonId
+            resetViews()
             if (selectedRequest == R.id.post_rb){
                 postRequest()
             }
@@ -39,11 +45,57 @@ class MainActivity : AppCompatActivity() {
 
     private fun getRequest(){
         Log.d(TAG, "---GET Request---")
+        // ---get reference to the views ---
+        //Edit Text
+        val urlInput = findViewById<EditText>(R.id.url_et).text.toString()
+        val headerInput = findViewById<EditText>(R.id.header_et).text.toString()
+        //Text Views
+        val responseCodeTv = findViewById<TextView>(R.id.responsecode_tv)
+        val headerTv = findViewById<TextView>(R.id.header_tv)
+        val bodyTv = findViewById<TextView>(R.id.body_tv)
+        val errorTv = findViewById<TextView>(R.id.errors_tv)
+
+        if(checkInternetConnection()){
+            val url = URL(urlInput)
+            //Create HTTP connection
+            val conn = url.openConnection() as HttpURLConnection
+
+            // Create Background Thread
+            val runnable = Runnable{
+
+            }
+            val thread= Thread(runnable)
+            thread.start()
+
+        }
+        else{
+            Log.d(TAG, "no internet connection")
+            errorTv.text= getString(R.string.no_internet_error)
+        }
     }
 
     private fun postRequest(){
         Log.d(TAG, "---POST Request---")
     }
+
+    private fun checkInternetConnection(): Boolean {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetwork != null
+    }
+
+    private fun resetViews(){
+        val responseCodeTv = findViewById<TextView>(R.id.responsecode_tv)
+        val headerTv = findViewById<TextView>(R.id.header_tv)
+        val bodyTv = findViewById<TextView>(R.id.body_tv)
+        val errorTv = findViewById<TextView>(R.id.errors_tv)
+
+        responseCodeTv.text = ""
+        headerTv.text = ""
+        bodyTv.text = ""
+        errorTv.text = ""
+
+    }
+
     companion object {
         private const val TAG = "MainActivity"
     }
